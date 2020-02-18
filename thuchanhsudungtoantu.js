cd2f5982abffcbc835cf4c4d669bdd9d72f19747
HaMinhKhue <khuesilicat@gmail.com>
2020-02-19 02:30:11 +0700

    mm

---
+++ thuchanhsudungtoantu.js
@@ -0,0 +1,6 @@
+iw=prompt('Nhập chiều ngang của hình chữ nhật');
+ih=prompt('Nhập chiều cao của hình chữ nhật');
+w=parseInt(iw);
+h=parseInt(ih);
+a=w*h;
+document.write('Diện tích của hình chữ nhật là '+a);
